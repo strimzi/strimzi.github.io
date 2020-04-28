ae8d1adbf7441fbc009d15409275341ddc907576
Jakub Scholz <www@scholzj.com>
2020-04-28 17:23:37 +0200

    Try to add namespaces to metadata
    Signed-off-by: Jakub Scholz <www@scholzj.com>

--- .functions/namespace/namespace.js
+++ .functions/namespace/namespace.js
@@ -1,11 +1,13 @@
 /* eslint-disable */
 
 const fetch = require('node-fetch')
+const yaml = require('js-yaml');
 
 exports.handler = async function(event, context) {
   try {
     // Get and validate the namespace parameter
     const namespace = event.queryStringParameters["namespace"]
+    
     if (namespace === undefined || namespace === null || namespace === '')  {
       console.log('Namespace is empty or not specified')
       throw "Namespace is empty or not specified"
@@ -15,16 +17,26 @@ exports.handler = async function(event, context) {
     
     // Fetch the latest installation YAMLs
     const response = await fetch('https://strimzi.io/install/latest')
+    
     if (!response.ok) {
       // NOT res.status >= 200 && res.status < 300
       return { statusCode: response.status, body: response.statusText }
     }
+    
     const data = await response.text()
 
+    // Change the namespaces
+    const fixedRbacNamespaces = data.replace(new RegExp('namespace: myproject', 'g'), 'namespace: ' + namespace)
+    let fixedAllNamespaces = String()
+    yaml.safeLoadAll(fixedRbacNamespaces, function (doc) {
+      doc.metadata.namespace = namespace
+      fixedAllNamespaces += yaml.safeDump(doc)
+    });
+    
     // Replace the namspace: myproject for namespace: whatever
     return {
       statusCode: 200,
-      body: data.replace(new RegExp('namespace: myproject', 'g'), 'namespace: ' + namespace)
+      body: fixedAllNamespaces
     }
   } catch (err) {
     // Handle errors
